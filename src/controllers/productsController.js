f80f68871233a8b1a00cf9812d3cbd2767423cf2
RODRIGO NEGRON <rodrigoo.n@hotmail.com.argit config --global user.email rodrigoo.n@hotmail.com.ar>
2021-02-02 16:01:30 -0300

    metodo delete agregado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -20,12 +20,25 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render("product-create-form")
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+
+		let newProduct ={
+			id: (products.length+1),
+			name: req.body.name,
+			price: req.body.price,
+			discount: req.body.discount,
+			category: req.body.category,
+			description: req.body.description
+		}
+
+		products.push(newProduct);
+		let productJson = JSON.stringify(products);
+		fs.writeFileSync('./src/data/productsDataBase.json', productJson);
+		res.redirect("/");
 	},
 
 	// Update - Form to edit
@@ -62,7 +75,12 @@ const controller = {
 
 	// Delete - Delete one product from DB
 	destroy : (req, res) => {
-		// Do the magic
+		let productToDelete=products.find(product=>(product.id==req.params.id));
+		products= products.filter(product=>(product.id != productToDelete.id));
+		let productJson = JSON.stringify(products);
+		fs.writeFileSync('./src/data/productsDataBase.json', productJson);
+
+		res.redirect("/");
 	}
 };
 
